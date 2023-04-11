8847599ce0b30d0ab20cd8f2ad2e6154285c4ead
TiyaniK <98035695+TiyaniK@users.noreply.github.com>
2023-04-11 16:45:00 +0200

    cookie

--- controllers/login.js
+++ controllers/login.js
@@ -35,20 +35,22 @@ const login = async (req, res) => {
       expiresIn: process.env.JWT_EXPIRATION,
     })
 
-  
-    res.cookie(String(existingUser.id), token, {
-      path: "/",
-      expires: new Date(Date.now() + 1000 * 30),
-      httpOnly: true,
-      sameSite: "lax",
-      secure: false,
-    });
-
-    return res.status(200).json({
+    res.status(200).json({
       message: "Successfully logged in",
       user: await User.findOne({ email }, "-password"),
       token: token,
     });
+
+  
+     return  res.cookie(String(existingUser.id), token, {
+      path: "/",
+      expires: new Date(Date.now() + 1000 * 30),
+      httpOnly: true,
+      sameSite: "none",
+      secure: false,
+    });
+
+ 
   } catch (error) {
     return res.status(500).json({ message: error.message })
   }
