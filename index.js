4f2c5c23a5325a6ce69a610d68af514373969d88
TiyaniK <98035695+TiyaniK@users.noreply.github.com>
2023-04-11 17:37:44 +0200

    cookie

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ import connectDB from "./database/db.js";
 import cors from "cors";
 
 const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
     credentials: true,
@@ -14,16 +12,10 @@ app.use(
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
-app.use(function(req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  next();
-});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 
 app.use("/", userRouter)
 
