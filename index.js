e041b4dbae962c4bf81d3ba0146a0f3d610a0cf1
TiyaniK <98035695+TiyaniK@users.noreply.github.com>
2023-04-11 17:21:19 +0200

    cookie

--- index.js
+++ index.js
@@ -14,6 +14,16 @@ app.use(
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
+app.use(function(req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
 
 app.use("/", userRouter)
 
